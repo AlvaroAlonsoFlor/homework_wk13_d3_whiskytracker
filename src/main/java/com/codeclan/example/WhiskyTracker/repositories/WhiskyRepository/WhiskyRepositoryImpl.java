3ba00c88cf79169521ab549bf1fca435ca17b6d5
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-24 17:10:17 +0100

    Add custom repo for whisky, allWhiskiesInYear, tested

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -1,5 +1,40 @@
 package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
+    //TODO: all whiskeys in a year
+    //TODO: all the whisky in a region
+    //TODO: all the whisky from a particular distillery that is a specific age
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Whisky> allWhiskiesInYear(int year) {
+        List<Whisky> result = null;
+        Session session = entityManager.unwrap(Session.class);
+
+        try{
+            Criteria cr = session.createCriteria(Whisky.class);
+            cr.add(Restrictions.eq("year", year));
+            result = cr.list();
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return result;
+    }
 }
